ccaf61c5a4483096797073e11f8c8681335d4ac1
Archit <arapaka@g.clemson.edu>
2017-06-21 08:35:34 -0500

    new sols

---
+++ algorithms/src/main/java/codefights/RemoveAdjacentDuplicatesString.java
@@ -0,0 +1,44 @@
+package codefights;
+
+/**
+ * Created by archithrapaka on 5/27/17.
+ */
+public class RemoveAdjacentDuplicatesString {
+
+    // removes
+    public static String removeDups(String s) {
+        char res[] = new char[s.length()];
+        int j = 0;
+        res[j] = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != res[j]) {
+                res[++j] = s.charAt(i);
+            } else {
+                res[j--] = '\u0000';
+            }
+        }
+
+        String result = String.valueOf(res);
+        return result.substring(0,j+1);
+    }
+
+    String removeDuplicateAdjacent(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        char previous = chars[0];
+        sb.append(chars[0]);
+        for(int i = 1 ; i < chars.length ; i++) {
+            if(chars[i] != previous) {
+                sb.append(chars[i]);
+                previous = chars[i];
+            }
+        }
+        String res = sb.toString();
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "azxxzy";
+        System.out.println(removeDups(s));
+    }
+}
---
+++ algorithms/src/main/java/codefights/SummaryArray.java
@@ -0,0 +1,22 @@
+package codefights;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by archithrapaka on 5/29/17.
+ */
+public class SummaryArray {
+
+//    static String[] composeRanges(int[] nums){
+//
+//    }
+
+    public static void main(String[] args) {
+        int nums[] = {3,7,9};
+//        String[] s = composeRanges(nums);
+//        for (String s1 :s) {
+//            System.out.println(s1);
+//        }
+    }
+}
